67b65f3a86ffd020cbc6b24814c92609827dafd0
mahek2745 <63193734+mahek2745@users.noreply.github.com>
2023-05-23 11:21:21 +0530

    Delete project/Beauty_parlour_ Bootstrap/assets directory

--- project/Beauty_parlour_ Bootstrap/assets/javascript_file/course_book_btn.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function () {
-    $("#submit").click(function () {
-        alert("Thank you!\n Your payment has been panding\n you can visit our parlour further details.");
-    });
-});
